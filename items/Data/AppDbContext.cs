489227cb2c268c2954a5bbbf786238261ad3ba9c
Ranajit Roy <ranajit.roy@colorado.edu>
2024-05-11 19:53:54 -0600

    changed to postgreSQL

---
+++ items/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace items.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
