453266c1e3e0cdcb93a037e094bda2e6353e5060
Ranajit Roy <ranajit.roy@colorado.edu>
2024-05-11 19:55:45 -0600

    User changed

---
+++ items/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace items.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public bool IsActive { get; set; }
+    }
+}
