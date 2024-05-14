7a6492338ab8618d0f1520d4d5f1cbfeb05cd7f1
Ranajit Roy <ranajit.roy@colorado.edu>
2024-05-13 23:00:27 -0600

    added dbcontext for users

---
+++ users/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using users.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace items.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            string connectionStr = String.Format("Host={0};Port={1};Database={2};Username={3};Password={4}",
+                configuration.GetValue<string>("PGSQL_HOST"),
+                configuration.GetValue<string>("PGSQL_PORT"),
+                configuration.GetValue<string>("PGSQL_DB"),
+                configuration.GetValue<string>("PGSQL_USER"),
+                configuration.GetValue<string>("PGSQL_PASSWORD")
+                );
+            optionsBuilder.UseNpgsql(connectionStr);
+
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
