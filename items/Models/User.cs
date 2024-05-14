b81785da7b83ead0ab5e24a1475470845e9063c7
Ranajit Roy <ranajit.roy@colorado.edu>
2024-05-13 23:03:59 -0600

    New User for changes for dbcontext in items service

--- items/Models/User.cs
+++ items/Models/User.cs
@@ -1,11 +1,13 @@
-﻿namespace items.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace items.Models
 {
     public class User
     {
         public Guid Id { get; set; }
-        public string Email { get; set; } = "";
-        public string FirstName { get; set; } = "";
-        public string LastName { get; set; } = "";
+        public required string Email { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
         public bool IsActive { get; set; }
     }
 }
