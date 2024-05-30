29bb9e6941655f480534c87d5a61b90540b6257c
Ranajit Roy <ranajit.roy@colorado.edu>
2024-05-29 21:09:46 -0600

    added new components to app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignOnPageComponent } from './sign-on-page/sign-on-page.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignOnPageComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
