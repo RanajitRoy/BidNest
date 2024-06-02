96cae5db7d93cb5793108ebb9d1fa3b3c5689904
Ranajit Roy <ranajit.roy@colorado.edu>
2024-06-01 20:08:39 -0600

    new modules imported

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { HttpClientModule } from '@angular/common/http';
+import { SignOnPageModule } from './sign-on-page/sign-on-page.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BodyComponent } from './body/body.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OAuthModule.forRoot(),
+    HttpClientModule,
+    SignOnPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
