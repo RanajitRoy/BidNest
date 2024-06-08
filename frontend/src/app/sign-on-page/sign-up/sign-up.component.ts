4f72d3ec6bb83a0f45670e5e0402b0457b4f4a23
Ranajit Roy <ranajit.roy@colorado.edu>
2024-06-08 03:16:47 -0600

    sign up component changed

--- frontend/src/app/sign-on-page/sign-up/sign-up.component.ts
+++ frontend/src/app/sign-on-page/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
-
+  signUpForm = new FormGroup({
+    firstNameField: new FormControl('', [Validators.minLength(2), Validators.maxLength(30), Validators.required]),
+    lastNameField: new FormControl('', [Validators.minLength(2), Validators.maxLength(30)]),
+    emailField: new FormControl('', [Validators.email, Validators.required]),
+    passwordField: new FormControl('', [Validators.required]),
+    cpasswordField: new FormControl('', [Validators.required])
+  })
 }
