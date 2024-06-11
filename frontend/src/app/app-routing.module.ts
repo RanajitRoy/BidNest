c0d14fb271cb6893ffc7d93e910f9061659dc4e8
Ranajit Roy <ranajit.roy@colorado.edu>
2024-06-10 23:57:34 -0600

    added route for sign on

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignOnPageComponent } from './sign-on-page/sign-on-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "signon", component: SignOnPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
