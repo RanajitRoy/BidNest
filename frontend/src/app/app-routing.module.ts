7a3ebc56b64d9cfb6baf3997cb0b8e16b2db0cad
Ranajit Roy <ranajit.roy@colorado.edu>
2024-06-20 23:38:59 -0600

    routes added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignOnPageComponent } from './sign-on-page/sign-on-page.component';
+import { ObjectsComponent } from './objects/objects.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 const routes: Routes = [
   {
     path: "signon", component: SignOnPageComponent, outlet: "main"
+  },
+  {
+    path: "objects", component: ObjectsComponent, outlet: "main"
+  },
+  {
+    path: "rooms", component: RoomsComponent, outlet: "main"
   }
 ];
 
