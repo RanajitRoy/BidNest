d7736f63abfe6a907a5bcc4388dca3f249c9c2d0
Ranajit Roy <ranajit.roy@colorado.edu>
2024-06-24 01:32:55 -0600

    updated rooms pages

--- frontend/src/app/rooms/rooms.component.ts
+++ frontend/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room } from 'src/shared/types';
 
 @Component({
   selector: 'app-rooms',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./rooms.component.scss']
 })
 export class RoomsComponent {
+  roomList: Room[] = [
+    {
+      id: '1',
+      name: 'room1',
+      description: "room 1 desc",
+      numOfObjects: 2,
+      numOfUsers: 3,
+      owner: '1'
+    },
+    {
+      id: '2',
+      name: 'room2',
+      description: "room 2 desc",
+      numOfObjects: 1,
+      numOfUsers: 2,
+      owner: '2'
+    },
+    {
+      id: '3',
+      name: 'room3',
+      description: "room 3 desc",
+      numOfObjects: 1,
+      numOfUsers: 3,
+      owner: '3'
+    }
+  ]
 
 }
